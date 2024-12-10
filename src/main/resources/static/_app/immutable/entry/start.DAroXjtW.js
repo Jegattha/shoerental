6b180463e148d409bdc5545642647cfc0b82bc82
Jegattha <jegattha@students.zhaw.ch>
2024-12-10 21:44:48 +0100

    wird automatisch ausgeführt

---
+++ src/main/resources/static/_app/immutable/entry/start.DAroXjtW.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.O6JQTNgi.js";export{t as start};
+//# sourceMappingURL=start.DAroXjtW.js.map
--- src/main/resources/static/_app/immutable/entry/start.Z11a6XdG.js
+++
@@ -1,2 +0,0 @@
-import{a as t}from"../chunks/entry.-1sZ8GMI.js";export{t as start};
-//# sourceMappingURL=start.Z11a6XdG.js.map
