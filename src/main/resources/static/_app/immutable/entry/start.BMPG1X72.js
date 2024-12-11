e9dadbd2d27b390fbaef08f987b18f1fb33a2cf5
Jegattha <jegattha@students.zhaw.ch>
2024-12-11 17:57:29 +0100

    automatisch

---
+++ src/main/resources/static/_app/immutable/entry/start.BMPG1X72.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.CHiHCOIf.js";export{t as start};
+//# sourceMappingURL=start.BMPG1X72.js.map
--- src/main/resources/static/_app/immutable/entry/start.BmTyDox-.js
+++
@@ -1,2 +0,0 @@
-import{a as t}from"../chunks/entry.FLcD_2XY.js";export{t as start};
-//# sourceMappingURL=start.BmTyDox-.js.map
