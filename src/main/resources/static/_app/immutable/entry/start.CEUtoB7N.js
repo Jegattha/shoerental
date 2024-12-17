73d542a79d87ab0ee027007690de8285965c6548
Jegattha <jegattha@students.zhaw.ch>
2024-12-17 20:40:50 +0100

    automatisch

---
+++ src/main/resources/static/_app/immutable/entry/start.CEUtoB7N.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.C-Me5MMH.js";export{t as start};
+//# sourceMappingURL=start.CEUtoB7N.js.map
--- src/main/resources/static/_app/immutable/entry/start.rNf8kF0R.js
+++
@@ -1,2 +0,0 @@
-import{a as t}from"../chunks/entry.BAQZEJpj.js";export{t as start};
-//# sourceMappingURL=start.rNf8kF0R.js.map
