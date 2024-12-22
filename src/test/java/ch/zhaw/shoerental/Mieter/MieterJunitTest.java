7819aa80db62e68241fb3d962510b9ce0e703fa0
Jegattha <jegattha@students.zhaw.ch>
2024-12-22 23:18:50 +0100

    closes #35

--- src/test/java/ch/zhaw/shoerental/Mieter/MieterJunitTest.java
+++ src/test/java/ch/zhaw/shoerental/Mieter/MieterJunitTest.java
@@ -35,7 +35,7 @@ public class MieterJunitTest {
 
         mieter.setOrt("Basel");
         assertEquals("Basel", mieter.getOrt());
-    }
+    }  
 
     @Test
     void testConstructorWithNonNullParameters() {
